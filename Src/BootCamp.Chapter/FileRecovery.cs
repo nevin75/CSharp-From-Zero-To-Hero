05dd3a3d22d64254bec9b3db330a2ccf663e7817
Nevin <Nevin.Danielson@NEVIND-T480S>
2020-02-11 12:53:26 -0600

    created FileRecovery class

---
+++ Src/BootCamp.Chapter/FileRecovery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class FileRecovery
+    {
+        public static void RemoveErrantCharacter()
+        {
+            const string newFile = @"C:\Git\CSharp-From-Zero-To-Hero\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input\Balances.repaired";
+
+            const string file = @"C:\Git\CSharp-From-Zero-To-Hero\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input\Balances.corrupted";
+
+            StreamWriter writer = new StreamWriter(newFile, true);
+            StreamReader reader = new StreamReader(file, true);
+
+            using (reader)
+            {
+                while (!reader.EndOfStream)
+                {
+                    char currentChar = (char)reader.Read();
+                    if (currentChar == '_')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        writer.Write(currentChar);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
