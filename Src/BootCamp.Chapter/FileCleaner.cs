42f52c74a848dd3a17206a0b2ae8ee9d61128fe6
Nevin <Nevin.Danielson@NEVIND-T480S>
2020-02-20 12:32:42 -0600

    clean corrupted file

--- Src/BootCamp.Chapter/FileCleaner.cs
+++ Src/BootCamp.Chapter/FileCleaner.cs
@@ -16,5 +16,32 @@ namespace BootCamp.Chapter
         {
             File.WriteAllText(cleanedFile, "a");
         }
+        public static void RemoveErrantCharacter()
+        {
+        
+            const string newFile = @"C:\Git\CSharp-From-Zero-To-Hero\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input\Balances.repaired";
+
+            const string file = @"C:\Git\CSharp-From-Zero-To-Hero\CSharp-From-Zero-To-Hero\Src\BootCamp.Chapter\Input\Balances.corrupted";
+
+            StreamWriter writer = new StreamWriter(newFile, true);
+            StreamReader reader = new StreamReader(file, true);
+
+            using (reader)
+            {
+                while (!reader.EndOfStream)
+                {
+                    char currentChar = (char)reader.Read();
+                    if (currentChar == '_')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        writer.Write(currentChar);
+                    }
+                }
+            }
+        }
     }
 }
+
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -11,6 +11,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
+            FileCleaner.RemoveErrantCharacter();
         }
     }
 }
